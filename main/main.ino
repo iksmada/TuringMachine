557e30c0fb7ec727ac07ea4601a16c82393558a4
Raphael Adamski <raphaeladamski@hotmail.com>
2015-12-11 15:31:45 -0200

    Codigo pratico

--- main/main.ino
+++ main/main.ino
@@ -8,7 +8,7 @@
 const int stepsPerRevolution = 24;
 rgb_lcd lcd;
 
-const int colorR = 255;
+const int colorR = 128;
 const int colorG = 0;
 const int colorB = 0;
 
@@ -39,16 +39,16 @@ void loop() {
   String hexa;
   
   //Print a message to the LCD.
-  lcd.print("initiating in ..."); //printa na linha 0
-  /*for (int i=TEMPOINICIO;i>0;i--){
+  lcd.print("initialization"); //printa na linha 0
+  for (int i=TEMPOINICIO;i>0;i--){
     lcd.setCursor(0, 1); //linha 1
     lcd.print(i);
     lcd.print("s remaining");
     delay(1000);
-  }*/
+  }
   lcd.print(0);
  
- for(int i = 0;i<16;i++){
+ for(int i = 0;i<8;i++){
   //Leitura
   valor = analogRead(sensor);
 
@@ -70,13 +70,15 @@ void loop() {
   lcd.print(decimal);
 
   lcd.setCursor(10, 1); //linha 1 penultima coluna
-  //lcd.print(decimal,HEX);
-  lcd.print(valor);
+  lcd.print(decimal,HEX);
+  //lcd.print(valor);
     
   //Gira o motor no sentido horario a 180 graus
   myStepper.step(6); // 12 = 24(passos por cliclo)/2
   delay(1000);
   
- } 
+ }
+ delay(3000);
+ lcd.clear(); 
 }
 
